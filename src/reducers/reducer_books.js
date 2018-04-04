6ed0db0def40fb8840515fb3f5168c5b8a7f5ede
js1 <jacob@slvr.mn>
2018-04-03 20:36:00 -0400

    working up to the end of section 4 lecture 40

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+//A reducer is a function that returns the application state.
+export default function(){
+    return [
+        { title: 'Javascript: The Good Parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
+    ]
+}
