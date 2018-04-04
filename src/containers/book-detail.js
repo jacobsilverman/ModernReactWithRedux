8da3e9fb398f15faf9084dbeedb45f753990976f
js1 <jacob@slvr.mn>
2018-04-04 16:13:12 -0400

    almost done

---
+++ src/containers/book-detail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class BookDetail extends Component {
+    render() {
+        return(
+            <div>Book Detail</div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        book: state.activeBook
+    };
+}
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,15 +1,24 @@
-import react, { Component } from 'react';
-
-export default class Booklist extends Component {
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+class BookList extends Component {
     renderList(){
         return this.props.books.map((book) => {
             return(
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li 
+                    key={book.title} 
+                    onClick={()=>this.props.selectBook(book)}
+                    className="list-group-item"
+                >
+                {book.title}
+                </li>
         );
     });
     }
 
     render(){
+        // console.log(this.props.books)
         return(
             <ul className="list-group col-sm-4">
                 {this.renderList()}
@@ -17,3 +26,18 @@ export default class Booklist extends Component {
         )
     }
 }
+
+function mapStateToProps(state){
+    /*Whatever is returned will show up as props inside of BookList */
+    return {
+        books: state.books
+    };
+}
+
+/* anything returned from this function will end up as props on the BookList container */
+function mapDispatchToProps(dispatch){
+    /* whenever selectBook is called, the result should be passed to all of our reducers */
+    return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+/* Promote BookList from a component to a container, it needs to know about this new dispatch method, selectBook. Make it available as a prop. */
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
